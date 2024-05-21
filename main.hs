dc5b59a3af80830d13656a1a46e2c4614534f2a6
a <anthonyleroy2017@gmail.com>
2024-05-21 17:00:16 +0900

    part 4

--- main.hs
+++ main.hs
@@ -5,15 +5,16 @@ import System.Environment (getArgs)
 import Data.Aeson (eitherDecode, FromJSON)
 import qualified Data.ByteString.Lazy as B
 import GHC.Generics
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 data MyData = MyData {
     name :: String,
-    alphabet :: [Char],
+    alphabet :: [String],
     blank :: Char,
     states :: [String],
     initial :: String,
-    finals :: [String],
-    --transitions :: [(String, Char, String, Char, String)]
+    finals :: [String]
 } deriving (Show, Generic)
 
 instance FromJSON MyData
@@ -29,12 +30,13 @@ main = do
             let jsonData = eitherDecode contents :: Either String MyData
             case jsonData of
                 Left err -> putStrLn $ "Error parsing JSON: " ++ err
-                Right data -> print data
-            putStrLn $ "Second argument: " ++ secondArg
+                Right myData -> do
+                    print myData
+                    putStrLn $ "Second argument: " ++ secondArg
 
-            let set1 = Set.fromList data.alphabet
-            let excludedChar = data.blank
-            let checkChar = \c -> Set.member c set1 && c /= excludedChar
-            if all checkChar secondArg
-                then putStrLn "All characters are in set1 and not equal to the excluded character"
-                else putStrLn "Some characters are not in set1 or are equal to the excluded character"
+                    let set1 = Set.fromList (concat (alphabet myData))
+                    let excludedChar = blank myData
+                    let checkChar = \c -> Set.member c set1 && c /= excludedChar
+                    if all checkChar secondArg
+                        then putStrLn "All characters are in the alphabet and not equal to the blank character"
+                        else putStrLn "Some characters are not in the alphabet or are equal to the blank character"
